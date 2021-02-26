42b406f711fccafc10bb51cd62d7e6aa1d89e895
putrii99 <79291938+putrii99@users.noreply.github.com>
2021-02-26 10:44:55 +0700

    nova nurul putri 1909291

---
+++ aktifitas4.py
@@ -0,0 +1,10 @@
+print("contoh perulangan suatu string")
+for huruf in "sistel":
+    print(huruf)
+    
+nilai_awal=0
+nilai_akhir=25 #tujuan/beberapa kali
+step=5 #jeda antar loop
+print("contoh perulanbgan angka dengan range()")
+for i in range(nilai_awal, nilai_akhir, step):
+    print(i)
