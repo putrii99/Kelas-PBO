96c9e15af4e1d8297807595723af041d58cd760b
putrii99 <79291938+putrii99@users.noreply.github.com>
2021-02-26 10:27:39 +0700

    Add files via upload

---
+++ aktifitas3.py
@@ -0,0 +1,14 @@
+#PERCABANGAN
+print("percabangan pada bahasa pemrograman python")
+print("pilih bahasa pemrograman: ")
+print("1. python")
+print("2. c")
+
+pilih=int(input("pilih menu 1/2: "))
+
+if pilih == 1:
+    print("bahasa pemrograman yang dipilih adalah python")
+elif pilih == 2:
+    print("bahasa pemrogaman yang dipilih adalah C")
+else :
+    print("pilihan anda tidak tersedia!!!")
