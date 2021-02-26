e7e2a2c5b10d21b03993688425c123b015a02f8e
putrii99 <79291938+putrii99@users.noreply.github.com>
2021-02-26 10:11:35 +0700

    Add files via upload

---
+++ aktifitas1.py
@@ -0,0 +1,12 @@
+#INPUT
+
+nama=input("masukkan nama anda:")
+umur=21
+
+#OUTPUT
+print("")
+print("=====INPUT DAN OUTPUT PADA BAHASA PEMROGRAMAN PYTHON=====")
+print("nama anda: ", nama)
+print("umur anda: ", umur)
+print("")
+
